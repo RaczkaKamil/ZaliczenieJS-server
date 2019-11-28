5c1c49c7316b4c20cd281c1e3711b6c0a0745e71
Kamil Rączka <raczka.kamil@onet.pl>
2019-11-28 21:22:59 +0100

    cd_day2

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,90 @@
 const express = require('express');
 const router = express.Router();
 const title = "GameNews";
-var arrayGame = [];
-arrayGame.push("#World of Warfraft!https://www.blizzard.com/static/_images/games/wow/wallpapers/wall1/wall1-1600x1200.jpg!Blizzard*MMORPG@Jej akcja toczy się cztery lata po wydarzeniach przedstawionych w grze Warcraft III: The Frozen Throne, w świecie stworzonym w 1994 roku na potrzeby Warcraft: Orcs & Humans. World of Warcraft jest czwartą (nie licząc dodatków) grą w uniwersum Warcrafta");
-arrayGame.push("#World of Tanks!https://i.wpimg.pl/O/644x361/d.wpimg.pl/169371567--387763345/world-of-tanks.png!Wargaming.net*MMO,symulacyjna,zręczościowa*Rozgrywka skupia się na bitwach pancernych pomiędzy graczami w interakcji player versus player z wykorzystaniem różnego typu wozów bojowych. Gracz ma do dyspozycji ponad 450 pojazdów: czołgów (lekkich, średnich, ciężkich), artylerii samobieżnej, pojazdów kołowych oraz niszczycieli czołgów z okresu od I wojny światowej aż po wczesną zimną wojnę, pochodzących ze Stanów Zjednoczonych, Czechosłowacji, ZSRR, III Rzeszy, Francji, Wielkiej Brytanii, Chin, Japonii, Szwecji, Włoch i Polski");
-arrayGame.push("#Metin2!https://cdn.gracza.pl/galeria_tv/hots/N640/296_280923324.jpg!Ymir Entertainment*MMORPG*Fabuła gry umiejscowiona jest w świecie fantasy inspirowanym kulturą Dalekiego Wschodu. „Kamienie Metin”, które spadły na ziemię spowodowały chaos w krainie, wojny pomiędzy królestwami oraz ataki zwierząt i potworów. Celem gry jest walka ze skutkami mrocznego wpływu kamieni.  ");
-arrayGame.push("#League of Legends!https://gamingsociety.pl/wp-content/uploads/2018/10/LoL.jpg!Riot Games*MOBA*sieciowa gra komputerowa z gatunku multiplayer online battle arena[13]. Powstała na bazie modyfikacji Defense of the Ancients (DotA) do Warcraft III: The Frozen Throne. Opracowana przez firmę Riot Games i początkowo wydana tylko dla systemu Windows. Została zapowiedziana 7 października 2008, a wydana 27 października 2009[6]. 15 lipca 2013 gra została uznana w USA za pełnoprawny sport  ");
+var fs = require('fs');
+var data = fs.readFileSync('games.json');
+var gamesList = JSON.parse(data);
+
+
+
 router.get('/', function(req, res, next) {
    res.render('index', { title, condition: true, anyArray: [1,2,3] });
  });
 
 
- router.get('/test/:id', function(req, res, next) {
-   res.render('test', {output: req.params.id});
- });
- 
  router.get('/game/all/', (req, res, next) =>{
   res.json({
-arrayGame
+    gamesList
  })
  });
 
+ router.get('/game/:id?', (req, res, next) =>{
+  var data = req.params;
+  var id = data.id;
+  console.log(id);
+  var game = (gamesList[id])
 
- router.get('/test2/', (req, res, next) =>{
- res.json({
-    'status': 'sukces',
-    'status2': 'sukces2'
+  res.json({
+   game
  })
- });
- 
- router.post('/test/submit', function(req, res, next) {
-   res.redirect('/test/');
+
+
  });
 
+ router.get('/add/:game',addGame);
+router.get('/delete/:id',deleteGame);
+
+function deleteGame(req,res){
+  var data = req.params;
+  var id = data.id;
+
+  var reply;
+  gamesList.splice(id,1);
+  var data = JSON.stringify(gamesList);
+  console.log(data);;
+  fs.writeFile('games.json', data, finished);
+
+  function finished( err)
+  {
+    console.log('all set');
+  }
+
+
+reply = {
+    msg: " deleted "
+  }
+  res.send(reply);
+}
+
+
+
+ function addGame (req,res){
+var data = req.params;
+var game = data.game;
+
+var reply;
+
+if( !game){
+  reply = {
+    msg: " Something is wrong"
+  }
+}else {
+  gamesList.push(game);
+  var data = JSON.stringify(gamesList);
+  console.log(data);;
+  fs.writeFile('games.json', data, finished);
+
+  function finished( err)
+  {
+    console.log('all set');
+  }
+
+reply={
+  msg: "all done"
+}
+}
+res.send(reply);
+
+ }
 
 module.exports = router;
